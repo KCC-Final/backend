de4ec96a50ab21c2a8479d52faa14c69d4548e5d
YunSung Choi <sunsu6765@gmail.com>
2025-10-23 11:54:49 +0900

    feat: 오늘의 한 문장 조회시 유저 검증

--- src/main/java/com/kcc/groo/config/SpringSecurityConfig.java
+++ src/main/java/com/kcc/groo/config/SpringSecurityConfig.java
@@ -16,45 +16,39 @@ import com.kcc.groo.jwt.JwtAuthFilter;
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
-	
-	@Autowired
-	JwtAuthFilter jwtAuthFilter;
-	
+
+    @Autowired
+    JwtAuthFilter jwtAuthFilter;
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
     }
-	
-	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-	    http.csrf((csrf)->csrf.disable());
-	    
-	    http.authorizeHttpRequests((authHttpReq) -> authHttpReq
-	            .requestMatchers(
-	                "/swagger-ui.html",
-	                "/swagger-ui/**",
-	                "/v3/api-docs/**",
-	                "/api-docs/**",
-	                "/actuator/**",
-	                "/api-docs/swagger-config",
-	                "/api/test/ping"
-	            ).permitAll()
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf((csrf) -> csrf.disable());
+
+        http.authorizeHttpRequests((authHttpReq) -> authHttpReq
+                .requestMatchers(
+                        "/swagger-ui.html",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**",
+                        "/api-docs/**",
+                        "/actuator/**",
+                        "/api-docs/swagger-config",
+                        "/api/test/ping"
+                ).permitAll()
 
                 // 로그인, 로그아웃, 토큰재발행 통과
-	            .requestMatchers(
-	                "/api/v1/auth/login",
-	                "/api/v1/auth/logout",
-	                "/api/v1/users/signup",
-	                "/api/v1/users/id/**",
-	                "/api/v1/users/password",
-	                "/api/v1/email",
-	                "/api/v1/email/verify",
-	                "/api/v1/quotes/daily",
-                    "/api/v1/token-refresh"
-	            ).permitAll()
+                .requestMatchers(
+                        "/api/v1/auth/login",
+                        "/api/v1/auth/logout",
+                        "/api/v1/token-refresh"
+                ).permitAll()
 
-                // 회원가입, ID/PW 찾기, 이메일 인증 통과
+                // 회원가입, 아이디 중복확인, ID/PW 찾기, 이메일 인증 통과
                 .requestMatchers(
                         "/api/v1/users/signup",
                         "/api/v1/users/id/**",
@@ -62,15 +56,15 @@ public class SpringSecurityConfig {
                         "/api/v1/email",
                         "/api/v1/email/verify"
                 ).permitAll()
-	            
-	            // 나머지는 인증 필요
-	            .anyRequest().authenticated()
-	    );
 
-	    http.sessionManagement((session)-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-	    http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-	    
-	    return http.build();
-	}
+                // 나머지는 인증 필요
+                .anyRequest().authenticated()
+        );
+
+        http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 
 }
